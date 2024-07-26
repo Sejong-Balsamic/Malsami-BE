b9172759d507d315e4e8195de98b5f1e82ad9733
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-27 02:11:22 +0900

    feat(security): Spring Security 설정 추가

---
+++ src/main/java/com/balsamic/sejongmalsami/common/config/WebSecurityConfig.java
@@ -0,0 +1,96 @@
+package com.balsamic.sejongmalsami.common.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+  private static final String[] AUTH_WHITELIST = {
+      "/", // 기본화면
+      "/api/**", //FIXME: 일시적으로 전체 API 주소 허용 (삭제해야함)
+      "/api/signup", // 회원가입
+      "/api/login", // 로그인
+      "/docs/**", // Swagger
+      "/v3/api-docs/**", // Swagger
+  };
+
+  private static final String[] ALLOWED_ORIGINS = {
+      "https://api.sejong-malsami.co.kr",
+      "https://www.sejong-malsami.co.kr",
+      "http://220.85.169.165:8086",
+      "http://220.85.169.165:3002",
+      "http://localhost:8080",
+      "http://localhost:3000"
+  };
+  @Bean
+  public WebSecurityCustomizer configure() {
+    return (web) -> web.ignoring()
+        .requestMatchers(new AntPathRequestMatcher("/static/**"));
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return
+        http.cors(cors -> cors
+                .configurationSource(corsConfigurationSource()))
+
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+
+            .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+//                .requestMatchers(HttpMethod.GET, "/api/my-page").hasRole("USER") //FIXME: 예시 페이지
+                .anyRequest().authenticated()
+            )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+            )
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+  }
+
+//  @Bean
+//  public AuthenticationManager authenticationManager(HttpSecurity http,
+//      BCryptPasswordEncoder bCryptPasswordEncoder,
+//      UserDetailsService UserDetailsService) throws Exception {
+//    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//    authProvider.setUserDetailsService(UserDetailsService);
+//    authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+//    return new ProviderManager(authProvider);
+//  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOriginPatterns(Arrays.asList(ALLOWED_ORIGINS));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(Collections.singletonList("*"));
+    configuration.setMaxAge(3600L);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+}
