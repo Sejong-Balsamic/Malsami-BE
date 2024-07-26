1b517cc0a7053d14987a7319224cf7276340e1d5
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-27 01:30:14 +0900

    init(project): SpringBoot Project Init

---
+++ src/main/java/com/balsamic/sejongmalsami/SejongMalsamiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.balsamic.sejongmalsami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongMalsamiBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SejongMalsamiBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/balsamic/sejongmalsami/SejongMalsamiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balsamic.sejongmalsami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongMalsamiBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
