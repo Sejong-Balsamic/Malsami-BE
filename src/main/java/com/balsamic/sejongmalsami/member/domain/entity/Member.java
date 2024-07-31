2cefb795750a9db3982030c7ddb82d4f0ffe16e7
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-31 13:55:53 +0900

    fix(member): MemberEntity 자동 id 증가 로직 추가, 로깅추가

--- src/main/java/com/balsamic/sejongmalsami/member/domain/entity/Member.java
+++ src/main/java/com/balsamic/sejongmalsami/member/domain/entity/Member.java
@@ -2,12 +2,15 @@ package com.balsamic.sejongmalsami.member.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Setter
@@ -15,8 +18,10 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Member {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long memberId;
 
   @Column(unique = true)
