102d56d2db8e1a99e75cfc954a130eefcd9830f6
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-31 15:05:46 +0900

    feat(entity): BaseTimeEntity 추가

--- src/main/java/com/balsamic/sejongmalsami/SejongMalsamiBackendApplication.java
+++ src/main/java/com/balsamic/sejongmalsami/SejongMalsamiBackendApplication.java
@@ -2,8 +2,10 @@ package com.balsamic.sejongmalsami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SejongMalsamiBackendApplication {
 
   public static void main(String[] args) {
