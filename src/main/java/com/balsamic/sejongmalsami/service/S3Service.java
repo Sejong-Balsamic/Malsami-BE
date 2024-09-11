a155af335679a52c058ac85db40c3a31f3b3c2b2
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-09-11 16:41:28 +0900

    fix(gitignore): application-prod.yml 제외 추가 (#8)
    * feat(S3): S3 환경변수, S3Config 설정
    
    * feat(S3): S3Service 작성
    
    * fix(gitignore): application-prod.yml 제외 추가
    
    * fix(S3): S3 Service 수정, S3 Service test 로그 확인
    
    * fix(yaml): application.yml 수정
    
    ---------
    
    Co-authored-by: SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>

---
+++ src/main/java/com/balsamic/sejongmalsami/service/S3Service.java
@@ -0,0 +1,47 @@
+package com.balsamic.sejongmalsami.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.balsamic.sejongmalsami.object.constants.ExtensionType;
+import com.balsamic.sejongmalsami.util.exception.CustomException;
+import com.balsamic.sejongmalsami.util.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class S3Service {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${S3_BUCKET_NAME}")
+    private String bucketName;
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename(); // 원본 파일 명
+        String extension = originalFilename.substring(originalFilename.lastIndexOf(".")); // 확장자 명
+
+        // 파일 확장자 검증
+        if (!ExtensionType.isValidExtension(extension)) {
+            throw new CustomException(ErrorCode.INVALID_FILE_FORMAT);
+        }
+
+        String fileName = bucketName + UUID.randomUUID() + "_" + originalFilename;
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        amazonS3Client.putObject(bucketName, originalFilename, file.getInputStream(), metadata);
+
+        return fileName;
+    }
+}
