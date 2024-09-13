3e78a5304f0413018d4cae998238721f08856b94
Baek Min Hong <minhong0620@gmail.com>
2024-09-13 18:08:23 +0900

    fix(course): Course 엔티티 수정

--- src/main/java/com/balsamic/sejongmalsami/object/Course.java
+++ src/main/java/com/balsamic/sejongmalsami/object/Course.java
@@ -1,26 +1,37 @@
 package com.balsamic.sejongmalsami.object;
 
+import com.balsamic.sejongmalsami.object.constants.Faculty;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Course {
+@ToString(callSuper = true)
+@SuperBuilder
+public class Course extends BaseTimeEntity{
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "course_id", columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
+  private UUID courseId;
 
-  private String subjectName;     // 교과목명
+  private String subject; // 교과목명
 
-  private String facultyName;     // 단과대학
+  @Enumerated(EnumType.STRING)
+  private Faculty faculty; // 단과대학
 
-  private String departmentName;  // 학과대학
+  private String department; // 학과
 }
