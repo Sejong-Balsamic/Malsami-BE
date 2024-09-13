e89ddb5a226fd53b07c5b1ac1cd9f91df480756d
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-09-13 21:36:22 +0900

    feat(course): 교과목 db 구현 (#27)
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11) (#12)
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#15)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10)
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#14)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11) (#12) (#13)
    
    * fix(dockefile): DockerFile 기본값 롤백
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거
    
    ---------
    
    Co-authored-by: SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#16)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11)
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#15)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10)
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#14)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11) (#12) (#13)
    
    * fix(dockefile): DockerFile 기본값 롤백
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거
    
    ---------
    
    Co-authored-by: SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
    
    * feat(cicd): PULL_REQUEST_TEMPLATE 추가 및 cicd test branch 푸쉬 테스트
    
    * feat(course): 교과목 db 구현 (#18) (#26)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11) (#12) (#13)
    
    * fix(dockefile): DockerFile 기본값 롤백
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#17)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11) (#12)
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#16)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11)
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#15)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10)
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거 (#14)
    
    * fix(cicd): application-prod.yml파일 build전 생성 (#10) (#11) (#12) (#13)
    
    * fix(dockefile): DockerFile 기본값 롤백
    
    * fix(cicd): application-prod.yml BASE64 인코딩 제거
    
    ---------
    
    
    
    ---------
    
    
    
    ---------
    
    
    
    ---------
    
    
    
    * feat(course): Course 엔티티 생성
    
    * feat(course): CourseRepository 생성
    
    * feat(git): ignore 추가
    
    * chore(gradle): 기존 DB 의존성 삭제, PG, Mongo 의존성 추가
    
    * chore(yml): application yml pg 설정추가
    
    * fix(s3): S3 Value 연결 수정 및 위치, 명명 개선
    
    * refactor(member): DB 변경으로 인한 기본 Entity 수정 및 로직 수정
    
    * fix(course): Course 엔티티 수정
    
    * feat(faculty): Faculty enum 생성
    
    * fix(course): CourseRepository 수정
    
    * chore(gradle): Apache 의존성 추가
    
    * feat(course): CourseService 생성
    
    * feat(resources): 2024-2 세종대학교 교과목명 엑셀 추가
    
    * feat(course): CourseController 생성
    
    ---------
    
    Co-authored-by: Baek Min Hong <128719380+minhong620@users.noreply.github.com>
    Co-authored-by: JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
    
    * refactor(git): gitignore에 cicd 폴더 추가
    
    * refactor(git) :remove GitHub workflows directory
    
    ---------
    
    Co-authored-by: JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
    Co-authored-by: Baek Min Hong <minhong0620@gmail.com>
    Co-authored-by: Baek Min Hong <128719380+minhong620@users.noreply.github.com>

---
+++ src/main/java/com/balsamic/sejongmalsami/object/Course.java
@@ -0,0 +1,37 @@
+package com.balsamic.sejongmalsami.object;
+
+import com.balsamic.sejongmalsami.object.constants.Faculty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@SuperBuilder
+public class Course extends BaseTimeEntity{
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "course_id", columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
+  private UUID courseId;
+
+  private String subject; // 교과목명
+
+  @Enumerated(EnumType.STRING)
+  private Faculty faculty; // 단과대학
+
+  private String department; // 학과
+}
