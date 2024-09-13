c829768763f8490dfceb0886627199b08e3964f8
Baek Min Hong <minhong0620@gmail.com>
2024-09-13 18:11:29 +0900

    fix(course): CourseRepository 수정

--- src/main/java/com/balsamic/sejongmalsami/repository/CourseRepository.java
+++ src/main/java/com/balsamic/sejongmalsami/repository/CourseRepository.java
@@ -1,10 +1,11 @@
 package com.balsamic.sejongmalsami.repository;
 
 import com.balsamic.sejongmalsami.object.Course;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course,Long> {
+public interface CourseRepository extends JpaRepository<Course, UUID> {
 
 }
