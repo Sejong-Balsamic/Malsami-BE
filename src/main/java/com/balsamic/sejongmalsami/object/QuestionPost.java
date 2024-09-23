93cc94c3896b5342f89d57c48fda19db0d1229aa
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-09-23 09:54:12 +0900

    fix(question-post): 질문게시판 entity, command, dto 수정

--- src/main/java/com/balsamic/sejongmalsami/object/QuestionPost.java
+++ src/main/java/com/balsamic/sejongmalsami/object/QuestionPost.java
@@ -2,12 +2,10 @@ package com.balsamic.sejongmalsami.object;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
 
 import java.util.UUID;
 
 @Entity
-@Setter
 @Getter
 @Builder
 @AllArgsConstructor
@@ -17,7 +15,7 @@ public class QuestionPost extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "question_post_id", columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
+    @Column(columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
     private UUID questionPostId;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -42,23 +40,21 @@ public class QuestionPost extends BaseTimeEntity {
     private String writer;
 
     // 조회 수
-    @ColumnDefault("0")
-    private int views;
+    @Builder.Default
+    private int views = 0;
 
-    // 좋아요 수 (일단 해놓고 몽고 좋아요 엔티티 설계후 수정할게요~)
-    @ColumnDefault("0")
-    private int likes;
+    // 좋아요 수
+    @Builder.Default
+    private int likes = 0;
 
-    // 댓글 수 (일단 해놓고 나중에 수정)
-    @ColumnDefault("0")
-    @Column(name = "answer_count")
-    private int answerCount;
+    // 댓글 수
+    @Builder.Default
+    private int answerCount = 0;
 
     // 엽전 현상금
     private int bounty;
 
     // 내 정보 비공개 여부
     @Builder.Default
-    @Column(name = "is_private")
     private Boolean isPrivate = false;
 }
