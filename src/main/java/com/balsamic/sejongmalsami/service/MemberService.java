48c2f472ec6500ab0fff6bffc3314325ee572ee9
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-09-24 00:18:35 +0900

    refactor(course): 주석 개선 순환참조 고침

--- src/main/java/com/balsamic/sejongmalsami/service/MemberService.java
+++ src/main/java/com/balsamic/sejongmalsami/service/MemberService.java
@@ -5,52 +5,105 @@ import com.balsamic.sejongmalsami.object.Member;
 import com.balsamic.sejongmalsami.object.MemberCommand;
 import com.balsamic.sejongmalsami.object.MemberDto;
 import com.balsamic.sejongmalsami.repository.MemberRepository;
+import com.balsamic.sejongmalsami.util.JwtUtil;
 import com.balsamic.sejongmalsami.util.SejongPortalAuthenticator;
 import com.balsamic.sejongmalsami.util.exception.CustomException;
 import com.balsamic.sejongmalsami.util.exception.ErrorCode;
+import io.jsonwebtoken.Claims;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MemberService implements UserDetailsService {
+
   private final MemberRepository memberRepository;
   private final SejongPortalAuthenticator sejongPortalAuthenticator;
+  private final JwtUtil jwtUtil;
 
+  /**
+   * Spring Security에서 사용자 정보를 로드하는 메서드
+   */
   @Override
-  public CustomUserDetails loadUserByUsername(String stringMemberId)
-      throws UsernameNotFoundException {
+  public CustomUserDetails loadUserByUsername(String stringMemberId) throws UsernameNotFoundException {
     UUID memberId;
     try {
       memberId = UUID.fromString(stringMemberId);
     } catch (IllegalArgumentException e) {
+      log.error("유효하지 않은 UUID 형식: {}", stringMemberId);
       throw new UsernameNotFoundException("유효하지 않은 UUID 형식입니다.");
     }
 
     Member member = memberRepository.findById(memberId)
-        .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        .orElseThrow(() -> {
+          log.error("회원 미발견: {}", memberId);
+          return new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+        });
     return new CustomUserDetails(member);
   }
 
-  public MemberDto createMember(MemberCommand command) {
+  /**
+   * 사용자 로그인 처리
+   */
+  @Transactional
+  public MemberDto signIn(MemberCommand command) {
     MemberDto dto = sejongPortalAuthenticator.getMemberAuthInfos(command);
+    Long studentId = Long.parseLong(dto.getStudentIdString());
 
-    Member savedMember = memberRepository.save(
-        Member.builder()
-            .studentId(Long.parseLong(dto.getStudentIdString()))
-            .studentName(dto.getStudentName())
-            .uuidNickname(UUID.randomUUID().toString().substring(0, 6))
-            .major(dto.getMajor())
-            .academicYear(dto.getAcademicYear())
-            .enrollmentStatus(dto.getEnrollmentStatus())
-            .build());
+    Member member = memberRepository.findByStudentId(studentId)
+        .orElseGet(() -> {
+          log.info("신규 회원 등록: studentId = {}", studentId);
+          return memberRepository.save(
+              Member.builder()
+                  .studentId(studentId)
+                  .studentName(dto.getStudentName())
+                  .uuidNickname(UUID.randomUUID().toString().substring(0, 6))
+                  .major(dto.getMajor())
+                  .academicYear(dto.getAcademicYear())
+                  .enrollmentStatus(dto.getEnrollmentStatus())
+                  .build());
+        });
+
+    if (member.getLastLoginTime() == null) {
+      log.info("신규 회원: studentId={}", studentId);
+    }
+
+    member.setLastLoginTime(LocalDateTime.now());
+    log.info("회원 로그인 완료: studentId={}", studentId);
+    memberRepository.save(member);
+
+    CustomUserDetails userDetails = new CustomUserDetails(member);
+    String accessToken = jwtUtil.createAccessToken(userDetails);
+    String refreshToken = jwtUtil.createRefreshToken(userDetails);
+
+    log.info("액세스 토큰 및 리프레시 토큰 생성 완료: 사용자={}", userDetails.getUsername());
+
     return MemberDto.builder()
-        .member(savedMember)
+        .member(member)
+        .accessToken(accessToken)
+        .refreshToken(refreshToken)
         .build();
+  }
+
+  /**
+   * JWT 토큰에서 Authentication 객체 생성
+   *
+   * @param token JWT 토큰
+   * @return Authentication 객체
+   */
+  public Authentication getAuthentication(String token) {
+    Claims claims = jwtUtil.getClaims(token);
+    String username = claims.getSubject();
+    CustomUserDetails userDetails = loadUserByUsername(username);
+    return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
   }
 }
--- src/main/java/com/balsamic/sejongmalsami/util/config/WebSecurityConfig.java
+++ src/main/java/com/balsamic/sejongmalsami/util/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.balsamic.sejongmalsami.util.config;
 
+import com.balsamic.sejongmalsami.service.MemberService;
 import com.balsamic.sejongmalsami.util.JwtUtil;
 import com.balsamic.sejongmalsami.util.filter.TokenAuthenticationFilter;
 import java.util.Arrays;
@@ -24,22 +25,26 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+/**
+ Spring Security 설정 클래스
+ */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
   private final JwtUtil jwtUtil;
+  private final MemberService memberService; // MemberService 주입
 
+  // 인증을 생략할 URL 패턴 목록
   private static final String[] AUTH_WHITELIST = {
       "/", // 기본화면
-      "/api/**", //FIXME: 일시적으로 전체 API 주소 허용 (삭제해야함)
-      "/api/signup", // 회원가입
+      "/api/member/signin", // 회원가입
       "/api/login", // 로그인
       "/docs/**", // Swagger
       "/v3/api-docs/**", // Swagger
-      "/**",
   };
 
+  // 허용된 CORS Origin 목록
   private static final String[] ALLOWED_ORIGINS = {
       "https://api.sejong-malsami.co.kr",
       "https://www.sejong-malsami.co.kr",
@@ -48,59 +53,71 @@ public class WebSecurityConfig {
       "http://localhost:8080",
       "http://localhost:3000"
   };
+
+  // 웹 보안 무시 설정 (정적 자원 등)
   @Bean
   public WebSecurityCustomizer configure() {
     return (web) -> web.ignoring()
         .requestMatchers(new AntPathRequestMatcher("/static/**"));
   }
 
-  @Bean
+  // 보안 필터 체인 설정
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return
-        http.cors(cors -> cors
-                .configurationSource(corsConfigurationSource()))
-
-            .csrf(AbstractHttpConfigurer::disable)
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-
-            .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers(AUTH_WHITELIST).permitAll()
-//                .requestMatchers(HttpMethod.GET, "/api/my-page").hasRole("USER") //FIXME: 예시 페이지
-                .anyRequest().authenticated()
-            )
-            .logout(logout -> logout
-                .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-            )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(new TokenAuthenticationFilter(jwtUtil, Arrays.asList(AUTH_WHITELIST)),
-                UsernamePasswordAuthenticationFilter.class)
-            .build();
+    return http
+        // CORS 설정
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        // CSRF 비활성화
+        .csrf(AbstractHttpConfigurer::disable)
+        // HTTP Basic 인증 비활성화
+        .httpBasic(AbstractHttpConfigurer::disable)
+        // 폼 로그인 비활성화
+        .formLogin(AbstractHttpConfigurer::disable)
+        // 요청에 대한 권한 설정
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(AUTH_WHITELIST).permitAll()
+            // .requestMatchers(HttpMethod.GET, "/api/my-page").hasRole("USER") //FIXME: 예시 페이지
+            .anyRequest().authenticated())
+        // 로그아웃 설정
+        .logout(logout -> logout
+            .logoutSuccessUrl("/login")
+            .invalidateHttpSession(true)
+        )
+        // 세션 관리 정책 설정 (STATELESS)
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        // JWT 인증 필터 추가
+        .addFilterBefore(new TokenAuthenticationFilter(jwtUtil, memberService, Arrays.asList(AUTH_WHITELIST)),
+            UsernamePasswordAuthenticationFilter.class)
+        .build();
   }
 
+  // 인증 매니저 설정
   @Bean
   public AuthenticationManager authenticationManager(HttpSecurity http,
       BCryptPasswordEncoder bCryptPasswordEncoder,
-      UserDetailsService UserDetailsService) throws Exception {
+      UserDetailsService userDetailsService) throws Exception {
     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(UserDetailsService);
+    authProvider.setUserDetailsService(userDetailsService);
     authProvider.setPasswordEncoder(bCryptPasswordEncoder);
     return new ProviderManager(authProvider);
   }
 
+  // CORS 설정 소스 빈
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOriginPatterns(Arrays.asList(ALLOWED_ORIGINS));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-    configuration.setAllowCredentials(true);
-    configuration.setAllowedHeaders(Collections.singletonList("*"));
-    configuration.setMaxAge(3600L);
+    configuration.setAllowedOriginPatterns(Arrays.asList(ALLOWED_ORIGINS)); // 허용된 Origin 패턴 설정
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); // 허용된 HTTP 메서드 설정
+    configuration.setAllowCredentials(true); // 자격 증명 허용
+    configuration.setAllowedHeaders(Collections.singletonList("*")); // 허용된 헤더 설정
+    configuration.setMaxAge(3600L); // 캐시 지속 시간 설정 (초 단위)
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
+    source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
     return source;
   }
+
+  // 비밀번호 인코더 빈 (BCrypt)
   @Bean
   public BCryptPasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
