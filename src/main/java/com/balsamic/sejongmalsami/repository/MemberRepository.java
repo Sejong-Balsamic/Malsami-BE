835f354337066e6714269110ab3e1d5856a64d8f
Baek Min Hong <minhong0620@gmail.com>
2024-09-23 23:21:00 +0900

    Revert "Squashed commit of the following:"
    This reverts commit 614653bb85aac74c6058b8ba5a424d68bbbda191.

--- src/main/java/com/balsamic/sejongmalsami/repository/MemberRepository.java
+++ src/main/java/com/balsamic/sejongmalsami/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package com.balsamic.sejongmalsami.repository;
 
 import com.balsamic.sejongmalsami.object.Member;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, UUID> {
 
 }
