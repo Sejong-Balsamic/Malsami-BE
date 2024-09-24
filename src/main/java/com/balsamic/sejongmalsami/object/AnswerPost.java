b8604adcffdec7a6ae2a66e48141856502978171
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-09-24 13:02:22 +0900

    feat(answer-post): 질문게시글 답변 엔티티 구현

---
+++ src/main/java/com/balsamic/sejongmalsami/object/AnswerPost.java
@@ -0,0 +1,48 @@
+package com.balsamic.sejongmalsami.object;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class AnswerPost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
+    private UUID answerPostId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private QuestionPost questionPost;
+
+    // 답변 내용
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    // 학과
+    @Column(nullable = false)
+    private String major;
+
+    // 좋아요 수 (추천 수)
+    @Builder.Default
+    private int likes = 0;
+
+    // 댓글 수
+    @Builder.Default
+    private int commentCount = 0;
+
+    // 답변 채택 여부
+    @Builder.Default
+    private Boolean isChaetaek = false;
+}
