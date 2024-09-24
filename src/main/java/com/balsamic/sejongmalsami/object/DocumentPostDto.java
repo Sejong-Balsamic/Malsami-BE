0a11897e247587bc3e040d6b8fed579567949beb
Baek Min Hong <minhong0620@gmail.com>
2024-09-24 13:57:24 +0900

    feat(document-post): DocumentPostDto 생성

---
+++ src/main/java/com/balsamic/sejongmalsami/object/DocumentPostDto.java
@@ -0,0 +1,14 @@
+package com.balsamic.sejongmalsami.object;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@Builder
+public class DocumentPostDto {
+
+}
