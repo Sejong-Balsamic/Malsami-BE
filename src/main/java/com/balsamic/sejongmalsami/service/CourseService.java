9ee0725d512df5f020011b815bec2e318941fd01
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-09-24 14:52:48 +0900

    fix(course): course 업로드 로직 개선 및 mulipart 설정 추가

--- src/main/java/com/balsamic/sejongmalsami/service/CourseService.java
+++ src/main/java/com/balsamic/sejongmalsami/service/CourseService.java
@@ -20,6 +20,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -30,6 +31,7 @@ public class CourseService {
   private final CourseRepository courseRepository;
 
   @Async
+  @Transactional
   public void parseAndSaveCourses(CourseCommand command){
     MultipartFile sejongCourseFile = command.getSejongCourseFile();
     String fileName = sejongCourseFile.getOriginalFilename();
@@ -60,21 +62,31 @@ public class CourseService {
 
     // MultipartFile을 InputStream으로 변환
     try (InputStream inputStream = sejongCourseFile.getInputStream()) {
+      log.info("교과목명 파일 InputStream 생성 성공");
       Workbook workbook = new XSSFWorkbook(inputStream);  // InputStream을 사용하여 Workbook 생성
       Sheet sheet = workbook.getSheetAt(0);  // 첫 번째 시트를 가져온다
 
       for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) { // 첫 번째 행은 헤더이므로 1부터 시작한다
         Row row = sheet.getRow(i);
 
-        Faculty faculty = Faculty.valueOf(row.getCell(0).getStringCellValue()); // 단과대학
-        String department = row.getCell(1).getStringCellValue(); // 학과
+        String facultyName = row.getCell(0).getStringCellValue(); // 단과대학명
+        String department = row.getCell(1).getStringCellValue(); // 학과명
         String subject = row.getCell(2).getStringCellValue(); // 교과목명
 
-        // 법학부 법학전공은 "대학" 이라고 EXCEL에 표기되어 있습니다
-        if(department.equals("법학부 법학전공")){
+        Faculty faculty;
+        // 단과대학명이 "대학"이라고 적힌 경우, "법학부 법학전공"으로 처리
+        if(facultyName.equals("대학")){
           faculty = Faculty.법학부법학전공;
+        } else {
+          try {
+            faculty = Faculty.valueOf(facultyName);  // 해당 enum을 찾는다
+          } catch (IllegalArgumentException e) {
+            log.error("잘못된 단과대학명: {}", facultyName);
+            throw new CustomException(ErrorCode.WRONG_FACULTY_NAME);
+          }
         }
-        // Course 객체를 생성하고 데이터를 저장한다
+
+        // Course 객체 생성 및 저장
         Course course = Course.builder()
             .faculty(faculty)
             .department(department)
@@ -83,14 +95,15 @@ public class CourseService {
             .semester(semester)
             .build();
 
-        log.info("교과목명 저장됨 : faculty: {}, department: {}, subject: {}", faculty, department, subject);
-        // 단과대학별 교과목명 저장
-        courseRepository.save(course);
+        log.info("교과목명 저장됨: faculty: {}, department: {}, subject: {}", faculty, department, subject);
+        courseRepository.save(course); // DB에 저장
       }
       workbook.close();
     } catch (IOException e) {
+      log.error("교과목명 파일 InputStream 생성 중 오류 발생", e);
       throw new CustomException(ErrorCode.COURSE_SAVE_ERROR);
     }
+    log.info("교과목명 파싱 완료됨: {}", sejongCourseFile.getOriginalFilename());
   }
 
   // 특정 단과대학에 해당하는 교과목명 목록을 반환하는 메서드
