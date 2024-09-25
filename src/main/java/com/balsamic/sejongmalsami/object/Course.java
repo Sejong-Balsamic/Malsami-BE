6cc24ca173e7e6d2be2fd360b032013f45812e6d
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-09-25 14:47:26 +0900

    feat(answer-post): 질문게시판 답변 엔티티 작성 (#53)

--- src/main/java/com/balsamic/sejongmalsami/object/Course.java
+++ src/main/java/com/balsamic/sejongmalsami/object/Course.java
@@ -17,10 +17,9 @@ import java.util.UUID;
 @ToString(callSuper = true)
 @SuperBuilder
 public class Course extends BaseEntity {
-
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "course_id", columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
+  @Column(columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
   private UUID courseId;
 
   private String subject; // 교과목명
@@ -29,4 +28,8 @@ public class Course extends BaseEntity {
   private Faculty faculty; // 단과대학
 
   private String department; // 학과
+
+  private Integer year; // 년도
+
+  private Integer semester; // 학기
 }
