8be0baf9f3fd802fec177879d8fa51aabd7ad2b3
Baek Min Hong <minhong0620@gmail.com>
2024-09-25 23:09:06 +0900

    refactor(document-post): DocumentPost 수정
    - nullable = false 삭제
    - 싫어요수 추가,
    - 내 정보 비공개를 내 학과 비공개로 변경
    - 작성자 member로 변경

--- src/main/java/com/balsamic/sejongmalsami/object/DocumentPostDto.java
+++ src/main/java/com/balsamic/sejongmalsami/object/DocumentPostDto.java
@@ -2,12 +2,10 @@ package com.balsamic.sejongmalsami.object;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @Getter
-@Setter
 @Builder
 public class DocumentPostDto {
 
