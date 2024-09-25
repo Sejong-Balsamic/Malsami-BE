fbbff07af9dfe3f4e956d0f0fbbddb81e3a46172
Baek Min Hong <minhong0620@gmail.com>
2024-09-25 23:12:11 +0900

    refactor(grade): 계급명 영어로 수정

--- src/main/java/com/balsamic/sejongmalsami/object/constants/Grade.java
+++ src/main/java/com/balsamic/sejongmalsami/object/constants/Grade.java
@@ -1,15 +1,8 @@
 package com.balsamic.sejongmalsami.object.constants;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum Grade {
-  COMMONER("천민"),
-  MIDDLE_CLASS("중인"),
-  NOBLE("양반"),
-  KING("왕");
-
-  private final String description;
+  천민,
+  중인,
+  양반,
+  왕
 }
