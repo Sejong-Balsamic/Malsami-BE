8115c2bb0d49eb111785b7e123d23d57074b6c2e
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2024-10-07 16:10:51 +0900

    feat(entity): MediaFile 엔티티 및 질문글, 답변글 이미지 파일 저장 로직 설계 (#134)

---
+++ src/main/java/com/balsamic/sejongmalsami/object/MediaFileCommand.java
@@ -0,0 +1,24 @@
+package com.balsamic.sejongmalsami.object;
+
+import com.balsamic.sejongmalsami.object.constants.MimeType;
+import com.balsamic.sejongmalsami.object.constants.PostType;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+@Builder
+@Getter
+@Setter
+@ToString
+public class MediaFileCommand {
+
+  private UUID questionId;
+  private MultipartFile file;
+  private String fileUrl;
+  private Long fileSize;
+  private PostType postType;
+  private MimeType mimeType;
+}
