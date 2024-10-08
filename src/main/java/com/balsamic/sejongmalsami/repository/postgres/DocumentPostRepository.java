287b9ce6d1e5ceaac556ae336630fdf0c7ee0281
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-08 10:58:15 +0900

    feat(document-post): 자료게시글 인기글 로직

---
+++ src/main/java/com/balsamic/sejongmalsami/repository/postgres/DocumentPostRepository.java
@@ -0,0 +1,15 @@
+package com.balsamic.sejongmalsami.repository.postgres;
+
+import com.balsamic.sejongmalsami.object.DocumentPost;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface DocumentPostRepository extends JpaRepository<DocumentPost, UUID> {
+
+  // 일간 인기글 계산 (글 작성일이 startDate 보다 나중인 질문 게시글)
+  @Query("SELECT p FROM DocumentPost p WHERE p.createdDate >= :startDate")
+  List<DocumentPost> findDocumentPostsAfter(LocalDateTime startDate);
+}
