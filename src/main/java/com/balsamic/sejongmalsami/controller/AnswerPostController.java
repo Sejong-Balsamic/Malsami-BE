9e2b1d31b46d1886d087a5727169633750065063
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-11 13:25:25 +0900

    feat(answer): 질문게시판 답변 글 등록 service, controller 작성

---
+++ src/main/java/com/balsamic/sejongmalsami/controller/AnswerPostController.java
@@ -0,0 +1,38 @@
+package com.balsamic.sejongmalsami.controller;
+
+import com.balsamic.sejongmalsami.object.AnswerPostCommand;
+import com.balsamic.sejongmalsami.object.AnswerPostDto;
+import com.balsamic.sejongmalsami.object.CustomUserDetails;
+import com.balsamic.sejongmalsami.service.AnswerPostService;
+import com.balsamic.sejongmalsami.util.log.LogMonitoringInvocation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/answer")
+@Tag(
+    name = "질문게시판 답변글 API",
+    description = "답변글 관련 API 제공"
+)
+public class AnswerPostController implements AnswerPostControllerDocs{
+
+  private final AnswerPostService answerPostService;
+
+  @Override
+  @PostMapping(value = "/post", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @LogMonitoringInvocation
+  public ResponseEntity<AnswerPostDto> saveAnswerPost(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails,
+      @ModelAttribute AnswerPostCommand command) {
+    command.setMemberId(customUserDetails.getMemberId());
+    return ResponseEntity.ok(answerPostService.saveAnswer(command));
+  }
+}
