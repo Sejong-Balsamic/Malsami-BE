819fa46507fdddfae1937d82f22b28fbaf4cbb7a
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-10-13 16:23:38 +0900

    feat(document): DocumentFile 추가 , DocumentPost 및 MediaFile 수정
    DocumentFile 엔티티 클래스 추가: 파일 정보 및 비밀번호 관련 필드 포함
    DocumentPost 엔티티 클래스 추가: 게시물 정보 및 관련 통계 필드 포함
    MediaFile 엔티티 클래스 추가: 미디어 파일 정보 및 MIME 타입 처리

--- src/main/java/com/balsamic/sejongmalsami/object/DocumentPost.java
+++ src/main/java/com/balsamic/sejongmalsami/object/DocumentPost.java
@@ -46,14 +46,14 @@ public class DocumentPost extends BaseEntity {
   @Enumerated(EnumType.STRING)
   private PostTier postTier = PostTier.CHEONMIN; // 게시물 등급
 
+  // 파일 미리보기 이미지 URL
+  private String previewUrl;
+
   @Builder.Default
   private Integer likeCount = 0; // 좋아요수
 
   @Builder.Default
   private Integer dislikeCount = 0; // 싫어요수
-
-  @Builder.Default
-  private Integer downloadCount = 0; // 다운로드수
 
   @Builder.Default
   private Integer commentCount = 0; // 댓글수
