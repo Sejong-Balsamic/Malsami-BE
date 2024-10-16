7d78130c836bb7f9959ef6844cab2f93cbdfb889
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-16 11:10:03 +0900

    feat(entity): 엽전, 경험치 엔티티 추가

---
+++ src/main/java/com/balsamic/sejongmalsami/object/Yeopjeon.java
@@ -0,0 +1,33 @@
+package com.balsamic.sejongmalsami.object;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Yeopjeon {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false)
+  private UUID YeopjeonId;
+
+  @OneToOne
+  private Member member;
+
+  // 총 엽전량
+  private Integer resultYeopjeon;
+
+}
