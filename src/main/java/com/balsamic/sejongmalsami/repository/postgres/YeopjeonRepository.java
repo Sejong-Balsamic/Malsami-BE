e9922fd7e7245e39e60122f5543c7f63f63280ab
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-16 14:15:20 +0900

    feat(repository): yeopjeon repository 추가

--- src/main/java/com/balsamic/sejongmalsami/repository/postgres/YeopjeonRepository.java
+++ src/main/java/com/balsamic/sejongmalsami/repository/postgres/YeopjeonRepository.java
@@ -1,9 +1,12 @@
 package com.balsamic.sejongmalsami.repository.postgres;
 
+import com.balsamic.sejongmalsami.object.Member;
 import com.balsamic.sejongmalsami.object.Yeopjeon;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface YeopjeonRepository extends JpaRepository<Yeopjeon, UUID> {
 
+  Optional<Yeopjeon> findByMember(Member member);
 }
