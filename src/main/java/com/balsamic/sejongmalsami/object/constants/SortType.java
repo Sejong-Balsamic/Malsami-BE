fb3a39b3617cd9e466435c7f35bd1c5d5530d0bb
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-11-06 13:02:49 +0900

    feat(enum): SortType description 수정

--- src/main/java/com/balsamic/sejongmalsami/object/constants/SortType.java
+++ src/main/java/com/balsamic/sejongmalsami/object/constants/SortType.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum SortType {
-  LATEST("최신 순"),
-  MOST_LIKED("좋아요 순"),
-  YEOPJEON_REWARD("엽전 현상금 순"),
-  VIEW_COUNT("조회수 순");
+  LATEST("최신순"),
+  MOST_LIKED("추천순"),
+  YEOPJEON_REWARD("엽전 현상금 높은 순"),
+  VIEW_COUNT("조회수 많은순");
 
   private final String description;
 }
