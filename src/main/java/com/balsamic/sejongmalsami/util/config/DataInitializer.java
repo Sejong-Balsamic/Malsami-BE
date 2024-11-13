47a6c8556997576d02d4777c299f3aafa78149e2
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-11-14 02:31:19 +0900

    refactor(course): 스프링 시작시 비동기 업데이트, 실행시간 표시

--- src/main/java/com/balsamic/sejongmalsami/util/config/DataInitializer.java
+++ src/main/java/com/balsamic/sejongmalsami/util/config/DataInitializer.java
@@ -7,6 +7,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -17,10 +21,33 @@ public class DataInitializer implements ApplicationRunner {
   // 애플리케이션이 시작될 때 실행
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    log.info("====== Server 시작: StartupRunner 실행 ======");
-    // Course 파싱
-    courseFileGenerator.initCourse();
-    // 저장된 Course -> Subject 파싱
-    courseFileGenerator.initSubject();
+    log.info("====== Server 시작: DataInitializer 실행 ======");
+    LocalDateTime overallStartTime = LocalDateTime.now();
+
+    // 초기화 시작 로그 추가
+    log.info("데이터 초기화를 시작합니다.");
+
+    // 비동기 : Course 파싱
+    CompletableFuture<Void> courseFuture = CompletableFuture.runAsync(() -> {
+      log.info("Course 초기화를 시작합니다.");
+      courseFileGenerator.initCourse();
+      log.info("Course 초기화가 완료되었습니다.");
+    });
+
+    // 비동기 : Course 저장 -> 이후 -> Subject 저장
+    courseFuture.thenRun(() -> {
+      log.info("Subject 저장 시작");
+      courseFileGenerator.initSubject();
+      log.info("Subject 초기화 완료");
+    }).thenRun(() -> {
+      LocalDateTime overallEndTime = LocalDateTime.now();
+      Duration overallDuration = Duration.between(overallStartTime, overallEndTime);
+      log.info("============ DB 세팅 완료 ==========");
+      log.info("총 소요 시간: {}초", overallDuration.getSeconds());
+      log.info("==================================");
+    }).exceptionally(e -> {
+      log.error("서버 시작 DB 세팅 중 오류 발생", e);
+      return null;
+    });
   }
 }
