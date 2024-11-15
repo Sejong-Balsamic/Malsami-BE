7952b63ec31170c6ca00a983735edc6db8455dcc
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-11-15 23:58:35 +0900

    fix(auth): logout AccessToken 헤더 제거

--- src/main/java/com/balsamic/sejongmalsami/controller/AuthController.java
+++ src/main/java/com/balsamic/sejongmalsami/controller/AuthController.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +51,10 @@ public class AuthController implements AuthControllerDocs {
     cookie.setMaxAge(0);
     cookie.setPath("/");
     response.addCookie(cookie);
+
+    // AccessToken 삭제
+    response.setHeader("Authorization", "Bearer ");
+
     return ResponseEntity.ok().build();
   }
 
