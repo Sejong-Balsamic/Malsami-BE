1d2171b66377dc5e6e8f0340dc96fdf898cf1fc3
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-11-15 11:33:26 +0900

    refactor(test): 테스트 로직 log 출력 업데이트

--- src/test/java/com/balsamic/sejongmalsami/util/LogUtilsTest.java
+++ src/test/java/com/balsamic/sejongmalsami/util/LogUtilsTest.java
@@ -1,9 +1,12 @@
 package com.balsamic.sejongmalsami.util;
 
+import com.balsamic.sejongmalsami.object.postgres.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class LogUtilsTest {
 
   @Test
@@ -13,12 +16,20 @@ class LogUtilsTest {
 
   void testLog() {
     String testMessage = "테스트 메시지";
+
     for (int i = 0; i < 6; i++) {
-      LogUtils.lineLog(testMessage);
+      LogUtils.lineLog(i+"번쨰"+testMessage);
     }
 
     LogUtils.superLog(testMessage);
+    // 빈 "=" 줄 출력
+    log.info("본문 내용");
+    LogUtils.lineLog(null);
+
+    LogUtils.superLog(
+        Member.builder()
+        .studentName("테스트이름 헬로")
+        .build());
 
   }
-
 }
