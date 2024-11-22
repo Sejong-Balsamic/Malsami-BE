b213223a6a8184049caa0f4439920b5ddc29eb4c
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-11-22 11:37:16 +0900

    feat(entity): 특정 글에 좋아요 누른 여부 isLiked 필드 추가

--- src/main/java/com/balsamic/sejongmalsami/repository/mongo/CommentLikeRepository.java
+++ src/main/java/com/balsamic/sejongmalsami/repository/mongo/CommentLikeRepository.java
@@ -1,10 +1,13 @@
 package com.balsamic.sejongmalsami.repository.mongo;
 
 import com.balsamic.sejongmalsami.object.mongo.CommentLike;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CommentLikeRepository extends MongoRepository<CommentLike, String> {
 
   Boolean existsByCommentIdAndMemberId(UUID commentId, UUID memberId);
+
+  List<CommentLike> findAllByCommentIdInAndMemberId(List<UUID> commentIds, UUID memberId);
 }
