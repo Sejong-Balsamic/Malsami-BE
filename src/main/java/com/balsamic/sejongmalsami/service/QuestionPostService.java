14d35f126b99ecca8c395d01ae68c2f58d7fccb8
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-11-22 09:21:41 +0900

    fix(question): 질문 글 조회시 isLiked 반환 오류 해결

--- src/main/java/com/balsamic/sejongmalsami/service/QuestionPostService.java
+++ src/main/java/com/balsamic/sejongmalsami/service/QuestionPostService.java
@@ -174,8 +174,9 @@ public class QuestionPostService {
   /**
    * <h3>특정 질문 글 조회 로직</h3>
    * <p>해당 글 조회 수 증가</p>
+   * <p>이미 좋아요 누른 글 isLiked 반환</p>
    *
-   * @param command postId
+   * @param command memberId, postId
    * @return
    */
   @Transactional
