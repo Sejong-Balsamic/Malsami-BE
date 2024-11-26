77ed4472a9390667cfed5acba1f17782fd7ac9e1
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-11-26 14:34:10 +0900

    feat(query): querycommand 기본 생성자 추가

--- src/main/java/com/balsamic/sejongmalsami/object/QueryCommand.java
+++ src/main/java/com/balsamic/sejongmalsami/object/QueryCommand.java
@@ -2,24 +2,25 @@ package com.balsamic.sejongmalsami.object;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Builder
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
-@NoArgsConstructor
 public class QueryCommand {
+
+  public QueryCommand() {
+    pageNumber = 0;
+    pageSize = 30;
+  }
 
   private String query;
   private String subject;
   @Schema(defaultValue = "0")
-  private Integer pageNumber = 0;
+  private Integer pageNumber;
   @Schema(defaultValue = "30")
-  private Integer pageSize = 30;
+  private Integer pageSize;
 }
