bda0b02285f00dfdf62cc7928df9a6e580879a5d
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2025-01-08 12:37:04 +0900

    [긴급][기능개선][엔티티] 게시글 엔티티 추상 클래스 (BaseEntity, BasePost) 리팩토링: refactor: DocumentRequestPost @Setter 어노테이션 추가 #746

--- src/main/java/com/balsamic/sejongmalsami/object/postgres/DocumentRequestPost.java
+++ src/main/java/com/balsamic/sejongmalsami/object/postgres/DocumentRequestPost.java
@@ -20,11 +20,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
+@Setter
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
