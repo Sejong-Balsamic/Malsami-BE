397aeaff6c830ab85d8bfe549e3b8d8e54d4eb7e
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-03-05 09:25:47 +0900

    refactor(cicd): ubuntu 최신버전 업그레이드 및 캐시 성능 최적화

--- src/test/java/com/balsamic/sejongmalsami/util/LogUtilsTest.java
+++ src/test/java/com/balsamic/sejongmalsami/util/LogUtilsTest.java
@@ -1,35 +1,23 @@
-//package com.balsamic.sejongmalsami.util;
-//
-//import com.balsamic.sejongmalsami.object.postgres.Member;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//@Slf4j
-//class LogUtilsTest {
-//
-//  @Test
-//  public void mainTest() {
-//    testLog();
-//  }
-//
-//  void testLog() {
-//    String testMessage = "테스트 메시지";
-//
-//    for (int i = 0; i < 6; i++) {
-//      LogUtils.lineLog(i+"번쨰"+testMessage);
-//    }
-//
-//    LogUtils.superLog(testMessage);
-//    // 빈 "=" 줄 출력
-//    log.info("본문 내용");
-//    LogUtils.lineLog(null);
-//
-//    LogUtils.superLog(
-//        Member.builder()
-//        .studentName("테스트이름 헬로")
-//        .build());
-//
-//  }
-//}
+package com.balsamic.sejongmalsami.util;
+
+import com.balsamic.sejongmalsami.util.log.LogUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+@Slf4j
+class LogUtilsTest {
+
+  @Test
+  public void mainTest() {
+    testLog();
+  }
+
+  void testLog() {
+    String testMessage = "테스트 메시지";
+    LogUtil.lineLog(testMessage);
+  }
+}
